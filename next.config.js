95f07229c44da2a43a606b82e4fe0890916646b0
Stavros <stavros.kefaleas@gmail.com>
2022-08-17 23:38:45 +0300

    build: implement storybook

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: function (config, options) {
+    config.experiments = {
+      resolve: {
+        fallback: {
+          util: require.resolve('util/')
+        }
+      }
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig;
